58af8cfd980f7f50d967bac47de429af8ba70e02
PRITISH <141925240+iam-pritish@users.noreply.github.com>
2023-09-26 16:11:34 +0530

    Create 9] Python_Tuple.py

---
+++ 9] Python_Tuple.py
@@ -0,0 +1,20 @@
+# Working with tuple is very similer as List.
+# Carete Empty tuple 
+
+t1 = ()
+
+# Let's Assing index value or append the tuple
+
+# t1[0] = 3
+# t1.append(5)
+# print(t1)    #>>>>>>> Tuple does not support item assignment or any updates
+
+t2 = (20,30,40,50,60)   #>>>> this tuple is fix no append,delete,update,concat
+
+print(len(t2))          # Length works
+#>>> 5
+
+print(t2[3])            # Indexing works
+#>>> 50
+
+# Slicing and Iteration also works 
