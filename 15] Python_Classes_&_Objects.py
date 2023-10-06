7e81ac521e32db5d3950fe7e803864a2c1ed53a9
PRITISH <141925240+iam-pritish@users.noreply.github.com>
2023-10-06 18:16:25 +0530

    Update 15] Python_Classes_&_Objects.py

--- 15] Python_Classes_&_Objects.py
+++ 15] Python_Classes_&_Objects.py
@@ -1,11 +1,14 @@
 # how to create a Class
 class Employee :
+    
     # Constructor of Class
     # It is mainly used for assignement of instance attribute
      def __init__(self, name, salary):
+        
         # Instance variable or instance Attribute
         self.emp_name = name
         self.emp_salary = salary
+    
     # Method of Class
      def DisplayEmployeeInfo(self):
         print("Emaployee Name:",self.emp_name,",Employee Salary:",self.emp_salary)
@@ -18,3 +21,6 @@ emp2.DisplayEmployeeInfo()
 
 print(emp1.emp_name)
 print(emp2.emp_name)
+
+
+# Difference between Class Varibles and Instance Variables?
