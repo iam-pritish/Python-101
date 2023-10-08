04a930d3b77d58e7f3cb19b50a38ff42074e7a3a
PRITISH <141925240+iam-pritish@users.noreply.github.com>
2023-10-08 12:01:07 +0530

    Create 0] Python_OOP's_Concepts_Theory.py

---
+++ 0] Python_OOP's_Concepts_Theory.py
@@ -0,0 +1,30 @@
+# What is Programming in General?
+# Programs are set of Rule or Instructions for manipulating Data in a meaningful ways.
+
+# In object orirnted Programmming [OOP's] Paradigm we can design the program using Class & Objects
+# The main Intension of getting these Classes and creating Objects is to, we can Re-use the code 
+# in differnt ways within code.
+
+# Major Principles are : { "CO-MIP-DE"}
+# 1] Class
+# 2] Object
+# 3] Method
+# 4] Inheritance 
+# 5] Polymorphism
+# 6] Data Abstraction
+# 7] Encapsulation 
+
+# Class :---> It is defined as COllection of OBjects
+# It is logical Entity Which has some specific Attributes and Methods
+# 
+# for example :- If you have Employee Class, Then It should contain Attributes and Method to 
+#                elaborate the specifictions of the Class (i.e. email_ID,Name,Age,Salary and so on...)
+# Here we can create Many and unique objects form a single class
+# Class Define a 'Type'
+# The process of creating an Object form a Class in called "Instantiantion"
+
+#           my_name = 'Virat'
+# Here we have creted a new single Varible called 'my_name' with the Value of 'Virat' in it
+# This Variable is actually a reffernce to an 'Object'
+# The type of Object is 'str' -because in order to creat it we Instatiationlized the in-built "str" class
+#  
